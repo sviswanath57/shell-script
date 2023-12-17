ee5f3a9d332af97fab24db94c461093df34a556f
sviswanath57 <88268124+sviswanath57@users.noreply.github.com>
2023-12-17 20:22:48 +0530

    added code

--- 02-nginx-install.sh
+++ 02-nginx-install.sh
@@ -4,9 +4,9 @@ N="\e[0m"
 Y="\e[33m"
 G="\e[32m"
 id=$(id -u)
-if [ $id eq 0 ]
+if [ $id ne 0 ]
 then
-    yum install ngnix
-else
     echo "Run with root a/c or get root access"
+else
+        yum install ngnix -y
 fi
