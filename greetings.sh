7c9d8229e5ff645fb63426a162b1815a85fad61c
s viswanath <s.viswanath57@gmail.com>
2023-12-20 03:49:18 +0530

    devops practice

---
+++ greetings.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+NAME=""
+WISHES=""
+
+USAGE(){
+    echo "USAGE:: ${basename $0} -n <name> -w <winshes>"
+    echo "Options::"
+    echo "-n give names"
+    echo "-w give wishes"
+}
+
+while getopts ":n:w:h" opt;do
+    case $opt in
+    n) NAME="$OPTORG";;
+    w) WISHES="$OPTORG";;
+    h) USAGE; exit;;
+    :) USAGE; exit;;
+    \?) echo "invalid options: -"$OPTARG"" >&2; USAGE; exit;;
+
+done
