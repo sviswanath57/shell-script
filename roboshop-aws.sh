a7b05753fd89cd06b16bd09200682b443a0cf9b6
s viswanath <s.viswanath57@gmail.com>
2023-12-20 02:15:46 +0530

    devops practice

--- roboshop-aws.sh
+++ roboshop-aws.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 AMI=ami-03265a0778a880afb
 SG_ID=sg-00c14e6d6ba354df2
-INSTANCES=("mangodb" "web")
+INSTANCES=("mangodb" "web" "user")
+ZONE_ID=Z0969811182YP4UP1MW89
+DOMAIN_NAME="devopslife.cloud"
 for i in "${INSTANCES[@]}"
 do
     if [ $i == "mangodb" ] || [ $i == "mysql" ]
@@ -10,13 +12,29 @@ do
     else
         INSTANCES_TYPE="t2.micro"     
     fi
-    aws ec2 run-instances \
+    Private_IP=$(aws ec2 run-instances \
         --image-id $AMI \
         --instance-type $INSTANCES_TYPE \
         --security-group-ids $SG_ID \
-        --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value='$i'}]'
-    Private_IP=$(aws ec2 describe-instances \
-            --query "Reservations[*].Instances[*].PrivateIpAddress" \
-            --output=text)
-    echo $i is $Private_IP
+        --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value='$i'}]' \
+        --query 'Instances[0].PrivateIpAddress' --output text)
+    echo "$i: $Private_IP"
+
+aws route53 change-resource-record-sets \
+  --hosted-zone-id $ZONE_ID \
+  --change-batch '
+{
+    "Comment": "Creating a record set for cognito endpoint",
+    "Changes":[{
+        "Action":"CREATE",
+        "ResourceRecordSet":{
+            "Name":"'$i'.'$DOMAIN_NAME'.",
+            "Type":"A",
+            "TTL":1,
+        "ResourceRecords":[{
+            "Value":"'$Private_IP'"}]
+    }}]
+}
+'
 done
+
