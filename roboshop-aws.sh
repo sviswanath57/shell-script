15519e6c9217cf4a98cdace18faeda9966716566
s viswanath <s.viswanath57@gmail.com>
2023-12-20 00:26:37 +0530

    devops practice

---
+++ roboshop-aws.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+AMI=ami-03265a0778a880afb
+SG_ID=sg-00c14e6d6ba354df2
+INSTANCES={"mangodb" "web" "mysql"}
+for i in "${INSTANCES[@]}"
+do
+    if [ $i == "mangodb" ] || [ $i == "mysql" ]
+    then
+        aws ec2 run-instances \
+        --image-id $AMI \
+        --instance-type t3.small \
+        --security-group-ids SG_ID \
+        --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value='$i'}]'
+    else
+        aws ec2 run-instances \
+        --image-id $AMI \
+        --instance-type t2.micro \
+        --security-group-ids SG_ID \
+        --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value='$i'}]'      
+    fi
+
+
+done
