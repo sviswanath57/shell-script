fd93ab3ec6122605d295755d1c94b8f48e5a19f4
s viswanath <s.viswanath57@gmail.com>
2023-12-20 21:15:01 +0530

    devops practice

---
+++ ifs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+FILE=/etc/passwd
+
+if [ ! -f $FILE ]
+then
+echo "Folder not exist"
+fi
+while IFS=":" read -r username password user_id group_id user_fullname home_dir shellpath
+do
+echo "User Name=$username"
+echo "Password=$password"
+echo "User_Id=$user_id"
+echo "Group ID=$group_id"
+#echo "username=$username"
+#echo "username=$username"
+
+done < $FILE
