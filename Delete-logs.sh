f5ed49fef7112a71d34695dc143cc9e05f3587a2
s viswanath <s.viswanath57@gmail.com>
2023-12-20 21:00:24 +0530

    devops practice

---
+++ Delete-logs.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+
+R="\e[31m"
+N="\e[0m"
+Y="\e[33m"
+G="\e[32m"
+
+DIR="/home/centos/oldlogs"
+if [ ! -d $DIR ]
+then
+echo "Folder not exist"
+fi
+
+LOG_FILES=$(find /home/centos/oldlogs -type f -mtime +14 -name "*.log")
+
+while IFS= read -r line
+do
+echo "delete log files $line"
+done << $LOG_FILES
