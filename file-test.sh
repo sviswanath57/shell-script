e3c588c1e3bebd2417007bdb92364bd9c57b25ba
s viswanath <s.viswanath57@gmail.com>
2024-01-29 12:52:26 +0530

    shell-script

--- file-test.sh
+++ file-test.sh
@@ -8,12 +8,16 @@
 #     mkdir $FOLDER
 #     echo "New folder created $FOLDER"
 # fi
+R="\e[31m"
+N="\e[0m"
+Y="\e[33m"
+G="\e[32m"
 
 FILE="file-testtt"
 if [ -f /devops/shell-script ];
 then
-    echo "file exist $FILE"
+    echo "$Y file exist $FILE $N"
 else
     touch $FILE
-    echo " New file created with name $FILE"
+    echo "$G New file created with name $FILE $N"
 fi
